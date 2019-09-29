c5490bd904a63cd0a28627cfe3bce15b13d740dd
sinsky <wesky93@gmail.com>
2019-09-30 00:17:02 +0900

    add daynamic slot

---
+++ dynamictest/src/components/Tree.vue
@@ -0,0 +1,16 @@
+<template>
+<ul>
+  <li v-for="s in subs" :class="s.type">
+    {{s.name}}
+    <tree :subs="s.subs"></tree>
+  </li>
+</ul>
+</template>
+
+<script>
+    export default {
+        name: "tree",
+        props:['subs']
+    }
+</script>
+
