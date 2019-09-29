91e722307082febe8396e4776e045c3af9e43388
sinsky <wesky93@gmail.com>
2019-09-29 13:26:09 +0900

    use slot scoped

---
+++ slottest/src/AppScoped.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="parent">
+    <child>
+      <template slot="type1" scope="p1">
+        <div>{{p1.cx}}+{{p1.cy}} = {{parseInt(p1.cx) +parseInt(p1.cy)}}</div>
+      </template>
+      <template slot="type2" scope="p2">
+        <div>{{p2.cx}} 더하기 {{p2.cy}} = {{parseInt(p2.cx) +parseInt(p2.cy)}}</div>
+      </template>
+    </child>
+  </div>
+</template>
+
+<script>
+    import Child from './components/ScopedSlot'
+
+    export default {
+        components: {Child}
+    }
+
+</script>
+
+<style>
+  .parent {
+    border: dashed 2px black;
+    padding: 5px;
+  }
+
+</style>
--- slottest/src/main.js
+++ slottest/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 // import App from './App.vue'
-import App from './AppNamed.vue'
+// import App from './AppNamed.vue'
+import App from './AppScoped'
 new Vue({
   el: '#app',
   render: h => h(App)
