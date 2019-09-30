e3c5ba9cd8aca06d52f939977149914a9b95ebb0
sinsky <wesky93@gmail.com>
2019-09-30 13:05:29 +0900

    만다라트 차트 기본틀 추가

---
+++ madalin/src/components/DetailChart.vue
@@ -0,0 +1,75 @@
+<template>
+  <table>
+    <tr>
+      <td>
+        <input-box v-model="a"></input-box>
+      </td>
+      <td>
+        <input-box v-model="b"></input-box>
+      </td>
+      <td>
+        <input-box v-model="c"></input-box>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input-box v-model="d"></input-box>
+      </td>
+      <td>
+        <input-box :value="$attrs.link" readonly></input-box>
+      </td>
+      <td>
+        <input-box v-model="e"></input-box>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <input-box v-model="f"></input-box>
+      </td>
+      <td>
+        <input-box v-model="g"></input-box>
+      </td>
+      <td>
+        <input-box v-model="h"></input-box>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+    import InputBox from "./InputBox";
+
+    export default {
+        components: {InputBox,},
+        props:['text'],
+        data() {
+            return {
+                a: "",
+                b: "",
+                c: "",
+                d: "",
+                e: "",
+                f: "",
+                g: "",
+                h: "",
+            }
+        },
+        methods:{
+            update(value){
+                this.$emit('input',value)
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+  table {
+    border-collapse: collapse;
+  }
+
+  th, td {
+    border: 1px solid #444444;
+    padding: 0px !important;
+  }
+</style>
---
+++ madalin/src/components/InputBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="text" class="box" :value="value" @input="updateValue($event.target.value)">
+</template>
+
+<script>
+    export default {
+        props:['value'],
+        methods:{
+            updateValue(value){
+                this.$emit('input',value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+  input {
+    border-radius: 0px !important;
+    border: none;
+    width: 70px!important;
+    height: 70px!important;
+    text-align: center;
+  }
+</style>
